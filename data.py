11ea031d2f209a3c34aafb95889c8288a8bfc10c
adiitiii2061 <aditi.meena@avantika.edu.in>
2026-03-20 08:56:38 +0000

    copy from task 1

---
+++ data.py
@@ -0,0 +1,43 @@
+# Integer
+a = 15
+print("Integer Before:", id(a))
+a = a + 5
+print("Integer After:", id(a))
+
+
+# Float
+f = 5.14
+print("\nFloat Before:", id(f))
+f = f + 1.0
+print("Float After:", id(f))
+
+
+# String
+s = "aditi"
+print("\nString Before:", id(s))
+s = s + " meena"
+print("String After:", id(s))
+
+
+# List
+lst = [1, 2, 3]
+print("\nList Before:", id(lst))
+lst.append(4)
+print("List After:", id(lst))
+
+
+# Tuple
+t = (1, 2, 3)
+print("\nTuple Before:", id(t))
+t = t + (4,)
+print("Tuple After:", id(t))
+
+
+# Set
+st = {1, 2, 3}
+print("\nSet Before:", id(st))
+st.add(4)
+print("Set After:", id(st))
+
+print("this is version 2")
+
